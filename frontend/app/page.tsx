af3f9213c56623eb2464fe5009cdbd3a87045318
superdev707 <superdev707@gmail.com>
2024-06-26 10:31:55 +0900

    Update: Fix Url

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,12 @@
-import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default function Home() {
-  return redirect("/webapp");
+  return (
+    <Link
+      href={"/webapp"}
+      className="text-center text-2xl font-bold text-blue-600 underline"
+    >
+      Go to event page
+    </Link>
+  );
 }
