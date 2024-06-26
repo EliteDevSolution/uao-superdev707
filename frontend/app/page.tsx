7a684b5d8f37e8f18e6da51b75a8c292531214b2
superdev707 <superdev707@gmail.com>
2024-06-26 09:48:13 +0900

    Update: Fix Url and Port

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
-import { CalendarEvent } from "@/components/CalenderEvent";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return <CalendarEvent />;
+  return redirect("/webapp");
 }
---
+++ frontend/app/webapp/page.tsx
@@ -0,0 +1,5 @@
+import { CalendarEvent } from "@/components/CalenderEvent";
+
+export default function Dashboard() {
+  return <CalendarEvent />;
+}
